aae2cfedebf31e44ebdfc315b357852c22180555
Kindred <lenatthomas16@gmail.com>
2023-03-15 14:14:11 +0530

    Added the config file that handles mongoDB connection

---
+++ server/config.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'vendor/autoload.php';
+
+class Manager{
+    private $connection;
+    function _construct(){
+        try{
+            $connection = new MongoDB\Client;
+        } catch (MongoDBDriverExceptionException $e){
+            echo $e->getMessage();
+            echo nl2br("n");
+        }
+    }
+    function getConnection(){
+        return $this->connection;
+    }
+}
+
+
+?>
