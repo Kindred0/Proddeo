b0e55b4224a0e4e5e007d55c7b704441d1a113b9
Kindred <lenatthomas16@gmail.com>
2023-03-30 23:51:28 +0530

    Indroduced a new Task Class

---
+++ server/TaskClass.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Task {
+    private $TaskID;
+    private $TaskName;
+    private $AssociatedProject;
+    private $Origin;
+    private $Assignedto;
+    private $TaskDescription;
+    private $StartDate;
+    private $Deadline;
+    private $Progress;
+    private $client;
+    private $collection;
+}
+
+
+
+?>
